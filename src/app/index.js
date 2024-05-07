e4b686e30af91804fa88c195053b77eb72a076a2
jottave <joao_victorflamengo10@outlook.com>
2024-05-07 10:47:15 -0300

    add handle error

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+require('express-async-errors');
+
 const routes = require('../routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
-
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.sendStatus(500);
+});
 app.listen(3000, () => console.log('Server is running'));
