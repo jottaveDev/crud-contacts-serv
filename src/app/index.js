65a044376819c9c26ca422dc38b9fc1655b70017
jottave <joao_victorflamengo10@outlook.com>
2024-05-19 20:05:05 -0300

    create middleware cors and errorHandler

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 require('express-async-errors');
 
+const cors = require('./middlewares/cors');
+const errorHandler = require('./middlewares/errorHandler');
 const routes = require('../routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
-app.use((error, req, res, next) => {
-  console.log(error);
-  res.sendStatus(500);
-});
+app.use(errorHandler);
+
 app.listen(3000, () => console.log('Server is running'));
---
+++ src/app/middlewares/errorHandler.js
@@ -0,0 +1,3 @@
+module.exports = (error, req, res, next) => {
+  res.sendStatus(500);
+};
